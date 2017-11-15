6bf6099962ceb6d3f17812950b6deb0b8ba48856
voodoo15 <arnold.redoblado@gmail.com>
2017-11-15 01:13:24 -0500

    Moved business info to App.js.  Created list of businesses using array.  Built functionality for the BusinessList component to iterate through the array and return Business components.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,13 +5,12 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
   render() {
     return (
-      <div class="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />;
+          })
+        }
       </div>
     )
   }
