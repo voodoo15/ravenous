44063e1cf515eaf1024b04304a9c8caebb86bf03
voodoo15 <arnold.redoblado@gmail.com>
2017-11-15 01:28:01 -0500

    Updated Business class, updating props.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,19 +6,19 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+            <p>{this.props.business.name}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipcode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>ITALIAN</h3>
-            <h3 className="rating">4.5 stars}</h3>
-            <p>90 reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
             </div>
         </div>
       </div>
